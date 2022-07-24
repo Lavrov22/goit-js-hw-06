69bfdf96e8dcbe3c27e10ae3f17d7ea1c9cdad55
Lavrov Andrey <Sattarlav@gmail.com>
2022-07-24 23:31:21 +0300

    day1

---
+++ js/task_2.js
@@ -0,0 +1,37 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const ulRef = document.querySelector('#ingredients');
+const firstItem = document.createElement('li');
+firstItem.textContent = ingredients[0];
+firstItem.classList.add('item');
+
+const secondItem = document.createElement('li');
+secondItem.textContent = ingredients[1];
+secondItem.classList.add('item');
+
+const thirdItem = document.createElement('li');
+thirdItem.textContent = ingredients[2];
+thirdItem.classList.add('item');
+
+const fourthItem = document.createElement('li');
+fourthItem.textContent = ingredients[3];
+fourthItem.classList.add('item');
+
+const fifthItem = document.createElement('li');
+fifthItem.textContent = ingredients[4];
+fifthItem.classList.add('item');
+
+const sixthItem = document.createElement('li');
+sixthItem.textContent = ingredients[5];
+sixthItem.classList.add('item');
+
+ulRef.append(firstItem, secondItem, thirdItem, fourthItem, fifthItem, sixthItem);
+
+console.log(ulRef.childNodes);
